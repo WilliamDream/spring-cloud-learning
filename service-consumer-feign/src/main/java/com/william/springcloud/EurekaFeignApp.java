b9b3620f425f1b1921ba229c8d5f4acfa2b16aca
williamdream <3339642010@qq.com>
2019-05-06 22:56:31 +0800

    feign

--- service-consumer-feign/src/main/java/com/william/springcloud/EurekaFeignApp.java
+++ service-consumer-feign/src/main/java/com/william/springcloud/EurekaFeignApp.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
- * Hello world!
+ * fegin是面向接口编程
  *
  */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages= {"com.william.springcloud"})
+@ComponentScan(basePackages= {"com.william.springcloud"})
 public class EurekaFeignApp 
 {
 	public static void main( String[] args ){
--- service-consumer-feign/src/main/java/com/william/springcloud/controller/UserInfoController.java
+++ service-consumer-feign/src/main/java/com/william/springcloud/controller/UserInfoController.java
@@ -2,16 +2,22 @@ package com.william.springcloud.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.william.springcloud.service.UserInfoService;
+import com.william.springcloud.service.UserService;
 
 
 @RestController
 public class UserInfoController {
 
-	private UserInfoService service;
+	/**
+	 * 使用接口的方式，面向接口编程调用服务
+	 * 区别于使用RibbOn+RestTemplate调用服务
+	 */
+	@Autowired
+	private UserService service;
 	
 	@GetMapping("/getlist")
 	public List<String> getUsers() {
